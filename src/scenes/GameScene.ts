5d35f4bc5e8a01dbcf35d0e4d01f57ea26ca9414
Denis <denis.zhuravkov@gmail.com>
2021-05-16 19:12:12 +0200

    Tower placing

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -2,7 +2,9 @@ import * as Phaser from 'phaser';
 import { CST } from "../constants"
 import EnemySpawner from '../models/EnemySpawner';
 import Level from '../models/Level';
+import Hud from '../view/Hud';
 import LevelView from '../view/LevelView';
+import Tower from "../models/Towers/Tower";
 
 export default class GameScene extends Phaser.Scene
 {
@@ -11,6 +13,7 @@ export default class GameScene extends Phaser.Scene
     private mEnemySpawner: EnemySpawner;
     private mLevel: Level;
     private mLevelView: LevelView;
+    private mHud: Hud;
 
     constructor()
     {
@@ -22,12 +25,15 @@ export default class GameScene extends Phaser.Scene
         this.mLevel = new Level();
         this.mLevelView = new LevelView(this, this.mLevel);
         this.mLevelView.loadView();
+        this.mHud = new Hud(this, this.mLevel);
+        this.mHud.makeHud();
+
         
         let graphics = this.add.graphics();
         graphics.lineStyle(3, 0xffffff, 1)
 
         this._createPath();
-        this._drawGrid(graphics);
+        //this._drawGrid(graphics);
 
         graphics.lineStyle(2, 0xFFFF00, 1);
         this.path.draw(graphics);
@@ -42,16 +48,14 @@ export default class GameScene extends Phaser.Scene
 
     private _drawGrid(graphics)
     {
-        let blocksW = this.renderer.width / CST.CELL_SIZE;
-        let blocksH = this.renderer.height / CST.CELL_SIZE;
         let cs = CST.CELL_SIZE;
 
         graphics.lineStyle(1, 0xFFFFFF, 0.8);
-        for(var i = 0; i < blocksH; ++i) {
+        for(var i = 0; i < CST.MAP_HEIGHT; ++i) {
             graphics.moveTo(0, i * cs);
             graphics.lineTo(this.renderer.width, i * 64);
         }
-        for(var j = 0; j < blocksW; ++j) {
+        for(var j = 0; j < CST.MAP_WIDTH; ++j) {
             graphics.moveTo(j * cs, 0);
             graphics.lineTo(j * cs, this.renderer.height);
         }
@@ -61,5 +65,12 @@ export default class GameScene extends Phaser.Scene
     update(time, delta)
     {
         this.mEnemySpawner.update(time, delta);
+    }
+
+    setTower(x, y, img){
+        let cs = CST.CELL_SIZE;
+
+        this.mLevel.setTower((x - cs / 2) / cs, (y - cs / 2) / cs);
+        let tower =  new Tower(this.add.image(x, y, img).setInteractive());   
     }
 }
